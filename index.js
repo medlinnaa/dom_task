fd2d6cffd89a029c9d714a3f3fa82d41f14d2dbc
medlinnaa <madinamamishova@gmail.com>
2023-10-21 06:09:49 +0400

    task is ready

---
+++ index.js
@@ -0,0 +1,17 @@
+let text=document.querySelector(".tay");
+
+setTimeout(()=>{
+  text.style.display="none";
+},2000);
+
+let myimg=document.querySelector(".myimage");
+
+setTimeout(()=>{
+    myimg.src="./taylor-swift-lover-sales-predictions.jpg";
+},2000 );
+
+let desc=document.querySelector(".desc");
+
+setTimeout(()=>{
+    desc.innerHTML="She said: The album started to organize itself in a way that made sense that :Lover: would be the flagship song of this album. It is a really romantic album, but it is also an album that celebrates love in all of its complexity.She said: The album started to organize itself in a way that made sense that :Lover: would be the flagship song of this album. It is a really romantic album, but it is also an album that celebrates love in all of its complexity.She said: The album started to organize itself in a way that made sense that :Lover: would be the flagship song of this album. It is a really romantic album, but it is also an album that celebrates love in all of its complexity.She said: The album started to organize itself in a way that made sense that :Lover: would be the flagship song of this album. It is a really romantic album, but it is also an album that celebrates love in all of its complexity.She said: The album started to organize itself in a way that made sense that :Lover: would be the flagship song of this album. It is a really romantic album, but it is also an album that celebrates love in all of its complexity.She said: The album started to organize itself in a way that made sense that :Lover: would be the flagship song of this album. It is a really romantic album, but it is also an album that celebrates love in all of its complexity."
+},3000)
